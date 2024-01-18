e39868510c80b6df3fe4313b45dd32d1a5e9d028
Ruslan <Giovingo92@gmail.com>
2024-01-18 22:09:03 +0200

    feat: module-02

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,27 @@
 import { FC } from "react";
 import { useHooks } from "../../helpers";
 
+import styles from "./Main.module.css";
+
 export const Main: FC = () => {
   const { navigate } = useHooks();
 
   return (
-    <>
-      <button type="button" onClick={() => navigate("/module-01")}>
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+        type="button"
+        onClick={() => navigate("/module-01")}
+      >
         Module-01
       </button>
-    </>
+      <button
+        className={styles.button}
+        type="button"
+        onClick={() => navigate("/module-02")}
+      >
+        Module-02
+      </button>
+    </div>
   );
 };
