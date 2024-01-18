c64d757841ab3d0ec733cd68d6d2c48b5e933b7c
Ruslan <Giovingo92@gmail.com>
2024-01-18 21:34:47 +0200

    feat: module-01

--- src/components/ModuleOne/ModuleOne.tsx
+++ src/components/ModuleOne/ModuleOne.tsx
@@ -1,20 +1,76 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
+
 import styles from "../ModuleOne/ModuleOne.module.css";
 
 export const ModuleOne: FC = () => {
+  const [items, setItems] = useState<string[]>([]);
+  const [gap, setGap] = useState<string>("");
+  const [columns, setColumns] = useState<string>("3");
+
+  const handleClick = () => {
+    setItems((prevState) => [...prevState, `${prevState.length + 1}`]);
+  };
+
+  const handleDelete = () => {
+    setItems((prevState) => prevState.slice(0, -1));
+  };
+
+  const onChangeGap = (e: ChangeEvent<HTMLInputElement>) => {
+    setGap(e.target.value);
+  };
+
+  const onChangeColumns = (e: ChangeEvent<HTMLInputElement>) => {
+    setColumns(e.target.value);
+  };
+
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.item}>1</div>
-        <div className={styles.item}>2</div>
-        <div className={styles.item}>3</div>
-        <div className={styles.item}>4</div>
-        <div className={styles.item}>5</div>
-        <div className={styles.item}>6</div>
-        <div className={styles.item}>7</div>
-        <div className={styles.item}>8</div>
-        <div className={styles.item}>9</div>
-        <div className={styles.item}>10</div>
+      <div
+        className={styles.container}
+        style={{
+          gap: `${gap}px`,
+          gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        }}
+      >
+        {items.map((item, index) => (
+          <div className={styles.item} key={index}>
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.buttonBox}>
+        <button className={styles.button} type="button" onClick={handleClick}>
+          Add item
+        </button>
+
+        <button className={styles.button} type="button" onClick={handleDelete}>
+          Delete item
+        </button>
+      </div>
+
+      <div className={styles.inputBox}>
+        <label className={styles.label}>
+          Gap:
+          <input
+            className={styles.input}
+            type="text"
+            onChange={onChangeGap}
+            value={gap}
+            placeholder="gap"
+          />
+        </label>
+
+        <label className={styles.label}>
+          grid-template-columns: repeat(?, 1fr);
+          <input
+            className={styles.input}
+            type="text"
+            onChange={onChangeColumns}
+            value={columns}
+            placeholder="columns with 1fr"
+          />
+        </label>
       </div>
     </>
   );
