01f845c14fcb3b9025e07940b51e0c3d7ace5a1a
Ruslan <Giovingo92@gmail.com>
2024-01-19 21:10:53 +0200

    feat: grid-column and grid-row

--- src/components/ModuleTwo/ModuleTwo.tsx
+++ src/components/ModuleTwo/ModuleTwo.tsx
@@ -5,7 +5,12 @@ import styles from "./ModuleTwo.module.css";
 export const ModuleTwo: FC = () => {
   return (
     <>
-      <div className={styles.title}>Module-02</div>
+      <div className={styles.container}>
+        <header className={`${styles.item} ${styles.header}`}>Header</header>
+        <main className={`${styles.item} ${styles.main}`}>Main</main>
+        <aside className={`${styles.item} ${styles.aside}`}>Aside</aside>
+        <footer className={`${styles.item} ${styles.footer}`}>Footer</footer>
+      </div>
     </>
   );
 };
