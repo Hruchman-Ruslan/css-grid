01bfb1249f41c7c494c137a0699b77730171753b
Ruslan <Giovingo92@gmail.com>
2024-01-22 17:36:24 +0200

    feat: posts section

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 
 import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
 
 export const Project: FC = () => {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 };
